f58e7fbf7733b5e20d7b2d9835d656223491d7de
nguyenxuanthuan.999@gmail.com <nguyenxuanthuan.999@gmail.com>
2020-06-04 12:42:34 +0700

    LTCSDL TK73

---
+++ LTCDSL.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LTCSDL.Web.Controllers
+{
+    using BLL;
+    using DAL.Models;
+    using Common.Req;
+    using System.Collections.Generic;
+    //using BLL.Req;
+    using Common.Rsp;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        public CategoriesController()
+        {
+            _svc = new CategoriesSvc();
+        }
+
+        [HttpPost("get-by-id")]
+        public IActionResult getCategoryById([FromBody]SimpleReq req)
+        {
+            var res = new SingleRsp();
+            res = _svc.Read(req.Id);
+            return Ok(res);
+        }
+
+        [HttpPost("get-all")]
+        public IActionResult getAllCategories()
+        {
+            var res = new SingleRsp();
+            res.Data = _svc.All;
+            return Ok(res);
+        }
+
+        [HttpPost("get-by-getCustOrderHist")]
+        public IActionResult getCustOrderHist([FromBody]SimpleReq req)
+        {
+            var res = new SingleRsp();
+            res.Data = _svc.getCustOrderHist(req.Keyword);
+            return Ok(res);
+
+        }
+
+        [HttpPost("get-by-GetCustOrdersDetail")]
+        public IActionResult GetCustOrdersDetail([FromBody]SimpleReq req)
+        {
+            var res = new SingleRsp();
+            res.Data = _svc.GetCustOrdersDetail(req.Id);
+            return Ok(res);
+        }
+
+        [HttpPost("get-by-GetCustOrdersOrders")]
+        public IActionResult GetCustOrdersOrders([FromBody]SimpleReq req)
+        {
+            var res = new SingleRsp();
+            res.Data = _svc.GetCustOrdersOrders(req.Keyword);
+            return Ok(res);
+        }
+
+        [HttpPost("get-by-getCustOrderHist_LinQ")]
+        public IActionResult getCustOrderHist_LinQ([FromBody]SimpleReq req)
+        {
+            var res = new SingleRsp();
+            res.Data = _svc.getCustOrderHist_LinQ(req.Keyword);
+            return Ok(res);
+        }
+
+        [HttpPost("get-by-getCustOrdersDetail_LinQ")]
+        public IActionResult getCustOrdersDetail_LinQ([FromBody]SimpleReq req)
+        {
+            var res = new SingleRsp();
+            res.Data = _svc.getCustOrdersDetail_LinQ(req.Id);
+            return Ok(res);
+        }
+        private readonly CategoriesSvc _svc;
+    }
+}
---
+++ LTCSDL.DAL/CategoriesRep.cs
@@ -0,0 +1,236 @@
+﻿using LTCSDL.Common.DAL;
+using System.Linq;
+
+namespace LTCSDL.DAL
+{
+    using Microsoft.Data.SqlClient;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+
+    public class CategoriesRep : GenericRep<NorthwindContext, Categories>
+    {
+        #region -- Overrides --
+
+        /// <summary>
+        /// Read single object
+        /// </summary>
+        /// <param name="id">Primary key</param>
+        /// <returns>Return the object</returns>
+        public override Categories Read(int id)
+        {
+            var res = All.FirstOrDefault(p => p.CategoryId == id);
+            return res;
+        }
+
+
+        /// <summary>
+        /// Remove and not restore
+        /// </summary>
+        /// <param name="id">Primary key</param>
+        /// <returns>Number of affect</returns>
+        public int Remove(int id)
+        {
+            var m = base.All.First(i => i.CategoryId == id);
+            m = base.Delete(m); //TODO
+            return m.CategoryId;
+        }
+
+        #endregion
+
+        
+
+        /// <summary>
+        /// Initialize
+        /// </summary>
+        /// 
+        public object getCustOrderHist(string cusId)       // ADO.DOT NET
+        {
+            List<object> res = new List<object>();      // res la result , ket qua
+            var cnn = (SqlConnection)Context.Database.GetDbConnection();
+            if (cnn.State == ConnectionState.Closed)
+                cnn.Open();
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataSet ds = new DataSet();
+                var cmd = cnn.CreateCommand();
+                cmd.CommandText = "CustOrderHist";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@CustomerID", cusId);
+                da.SelectCommand = cmd;
+                da.Fill(ds);        // do du lieu vao
+                if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)  // Kiem tra co du lieu hay khong
+                {
+                    foreach (DataRow row in ds.Tables[0].Rows)
+                    {
+                        var x = new
+                        {
+                            ProductName = row["ProductName"],
+                            Total = row["Total"]
+                        };
+                        res.Add(x);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                res = null;
+            }
+            return res;
+        }
+
+        public object GetCustOrdersDetail(int ordId)       // ADO.DOT NET
+        {
+            List<object> res = new List<object>();      // res la result , ket qua
+            var cnn = (SqlConnection)Context.Database.GetDbConnection();
+            if (cnn.State == ConnectionState.Closed)
+                cnn.Open();
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataSet ds = new DataSet();
+                var cmd = cnn.CreateCommand();
+                cmd.CommandText = "CustOrdersDetail";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@OrderID", ordId);
+                da.SelectCommand = cmd;
+                da.Fill(ds);        // do du lieu vao
+                if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)  // Kiem tra co du lieu hay khong
+                {
+                    foreach (DataRow row in ds.Tables[0].Rows)
+                    {
+                        var x = new
+                        {
+                            ProductName = row["ProductName"],
+                            UnitPrice = row["UnitPrice"],
+                            Quantity = row["Quantity"],
+                            Discount = row["Discount"],
+                            ExtendedPrice = row["ExtendedPrice"]
+
+                        };
+                        res.Add(x);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                res = null;
+            }
+            return res;
+        }
+
+        public object GetCustOrdersOrders(string ordId)       // ADO.DOT NET
+        {
+            List<object> res = new List<object>();      // res la result , ket qua
+            var cnn = (SqlConnection)Context.Database.GetDbConnection();
+            if (cnn.State == ConnectionState.Closed)
+                cnn.Open();
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataSet ds = new DataSet();
+                var cmd = cnn.CreateCommand();
+                cmd.CommandText = "CustOrdersOrders";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@CustomerID", ordId);
+                da.SelectCommand = cmd;
+                da.Fill(ds);        // do du lieu vao
+                if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)  // Kiem tra co du lieu hay khong
+                {
+                    foreach (DataRow row in ds.Tables[0].Rows)
+                    {
+                        var x = new
+                        {
+                            OrderID = row["OrderID"],
+                            OrderDate = row["OrderDate"],
+                            RequiredDate = row["RequiredDate"],
+                            ShippedDate = row["ShippedDate"],
+
+
+                        };
+                        res.Add(x);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                res = null;
+            }
+            return res;
+        }
+
+        public object getCustOrderHist_LinQ(string cusId)       // ADO.DOT NET LIN Q
+        {
+            var res = Context.Products
+                .Join(Context.OrderDetails, a => a.ProductId, b => b.ProductId, (a, b) => new
+                {
+                    a.ProductId,    // Lay cac Trường trong Stored
+                    a.ProductName,
+                    b.Quantity,
+                    b.OrderId
+                })
+                .Join(Context.Orders, a => a.OrderId, b => b.OrderId, (a, b) => new
+                {
+                    a.ProductId,
+                    a.ProductName,
+                    a.Quantity,
+                    a.OrderId,
+                    b.CustomerId
+                }).Where(x=>x.CustomerId == cusId)
+                .ToList();
+
+            var data = res.GroupBy(x => x.ProductName).Select(x => new
+            {
+                ProductName = x.First().ProductName,    // các trường select trong Stored
+                Total = x.Sum(p => p.Quantity)
+            });
+
+            return data;    // Return kiểu dữ liệu trả về
+            
+        }
+
+        public object getCustOrdersDetail_LinQ(int ordId)       // ADO.DOT NET LIN Q
+        {
+            var res = Context.Products
+                .Join(Context.OrderDetails, a => a.ProductId, b => b.ProductId, (a, b) => new       // Ket ProductId
+                {
+                    a.ProductId,    // Lay cac Trường trong Stored
+                    a.ProductName,
+                    b.UnitPrice,
+                    b.Quantity,
+                    b.Discount,
+                    ExtendedPrice = b.Quantity * (1 - (decimal)b.Discount) * b.UnitPrice,   // Ep sang kieu decimal
+                    b.OrderId
+                }).Where(x => x.OrderId == ordId)       // Ket OrderID
+                .ToList();
+
+            // Co Group By thi co dong duoi
+            //var data = res.Select(x => new
+            //{
+            //    ProductName = x.ProductName,    // các trường select trong Stored
+            //    UnitPrice =  x.UnitPrice,
+            //    Quantity = x.Quantity,
+            //    Discount = x.Discount,
+            //    x.ExtendedPrice
+
+            //});
+            //return Data;
+
+            return res;    // Return kiểu dữ liệu trả về
+
+        
+        }
+
+    }
+
+}
+
